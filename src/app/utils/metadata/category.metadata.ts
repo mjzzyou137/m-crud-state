5c7e5cb7325b96a1050541afa4872576a9c8bc08
NguyenTheMan <luvzk12345@gmail.com>
2020-09-21 00:47:54 +0700

    m

--- src/app/utils/metadata/category.metadata.ts
+++ src/app/utils/metadata/category.metadata.ts
@@ -1,7 +1,7 @@
 import { Metadata, HttpRequestMethod } from "m-crud-state";
 import { EntityName } from "../entity-name";
 
-const baseUrl = "http://elearning0706.cybersoft.edu.vn/api";
+const baseUrl = "https://elearning0706.cybersoft.edu.vn/api";
 
 export const categoryMetadata: Metadata = {
   entityName: EntityName.DanhMucKhoaHoc,
